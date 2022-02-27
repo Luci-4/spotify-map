348c777878d04afc1f30199fe7dc21cb206bb215
wojci <wojciech.kurdybacha@gmail.com>
2022-02-27 04:21:21 +0100

    setup access_token obtainment without auth

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,32 +1,24 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').config();
+var axios = require('axios').default;
 const spotify_my_client_id = process.env.SPOTIFY_MY_CLIENT_ID;
 const spotify_my_client_secret = process.env.SPOTIFY_MY_CLIENT_SECRET;
 var access_token;
-const rootUrl = "";
 
-router.get("/access/:redirecttarget/:code", async function(req, res) {
-    const redirectTarget = req.params.redirecttarget
-    const code = req.params.code;
-
-    const url = `https://accounts.spotify.com/api/token`
-
-    const headers = {
-        headers: {
-
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-    };
-    let data = {
-
-        grant_type: "authorization_code",
-        code: code,
-        redirect_uri: redirectTarget,
+router.get('/access', function(req, res) {
+    const url =  'https://accounts.spotify.com/api/token'
+    const data = {
+        grant_type: 'client_credentials',
         client_id: spotify_my_client_id,
         client_secret: spotify_my_client_secret
     }
+    const headers = {
+        headers: {
 
+        "Content-Type": "application/x-www-form-urlencoded",
+        }
+    }
     const authRes = axios
         .post(
             url,
@@ -34,27 +26,12 @@ router.get("/access/:redirecttarget/:code", async function(req, res) {
             headers
         )
         .then(response => {
-            access_token = response.data.access_token;
+            access_token = response.data.access_token
             res.send()
         })
         .catch(error => {
+            console.log(error)
             res.status(502).send()
         })
 })
-
-router.get('/login/:redirecttarget', function(req, res) {
-    const redirectTarget = req.params.redirecttarget
-    console.log(redirectUri + redirectTarget)
-    var scopes = 'user-read-private playlist-modify-private playlist-read-private user-read-email';
-    var redirectUrl = 'https://accounts.spotify.com/authorize' +
-    '?response_type=code' +
-    '&client_id=' + spotify_my_client_id +
-    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-    '&redirect_uri=' + encodeURIComponent(rootUrl + redirectTarget);
-    const res_obj = {
-      url: redirectUrl
-    }
-    res.json(res_obj)
-})
-
 module.exports = router;
