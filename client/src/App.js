a2eb316ec248a94dbfcef60ad3e66ded69b34ef7
unknown <pigor2400@gmail.com>
2022-04-04 21:54:42 +0200

    big changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,7 @@
-import './App.css'
-import Search from './components/Search'
-import UserInput from './components/UserInput'
-import AllRelated from './components/AllRelated'
-import React, { useState, useEffect } from 'react'
-
 function App() {
-    const url = 'https://spotify-map.herokuapp.com/spotify/access'
-    const [token, setToken] = useState('')
-    const [query, setQuery] = useState('')
-    const [resID, setID] = useState(null)
-
-    useEffect(() => {
-        ;(async () => {
-            const res = await fetch(url)
-            const access_token = await res.text()
-            setToken(access_token)
-        })()
-    }, [])
-
     return (
-        <div id="App">
-            <header>
-                <h1>Find your new love babe!</h1>
-                <div>
-                    <UserInput setQuery={setQuery} />
-                    <Search query={query} access_token={token} setID={setID}/>
-                </div>
-            </header>
-            <main>
-                <AllRelated id={resID} access_token={token}/>
-            </main>
+        <div id='App'>
+            <h1>Goodbye world!</h1>
         </div>
     )
 }
